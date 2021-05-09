4522fae70dce984a38f2df1d2e9c2d83b2806b1f
Hao <kevinoctaviushao@gmail.com>
2021-05-09 18:51:57 +0700

    Design Master User

--- Proyek/obj/Debug/Proyek_Content.g.i.cs
+++ Proyek/obj/Debug/Proyek_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("loginbackground.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("homepagemaster.jpg")]
 
 
